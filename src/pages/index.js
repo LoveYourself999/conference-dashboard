4a7b6124e1a2d7a50bf08f220d67bc0d65eceae2
ShangenC <54390035+LoveYourself999@users.noreply.github.com>
2024-05-02 13:59:58 -0400

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
 
   useEffect(() => {
     // Fetch the CSV data from the server
-    fetch('/api/data')
+    fetch('/conference-dashboard/api/data')
       .then((response) => response.text())
       .then((csvData) => setData(csvData));
   }, []);
